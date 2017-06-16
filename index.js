828f5f03398deb1edf04cae630dccbef98f2a9bf
Yi Zhao <zhaoyi0113@gmail.com>
2017-06-16 10:24:53 +1000

    update version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Created by joey on 15/6/17.
  */
 
-const {launchMongoInstance, launchSingleInstance, killMongoInstance, launchReplicaSet, launchMongos, getRandomPort, generateMongoData} = require('src/mlaunch_utils');
+var {launchMongoInstance, launchSingleInstance, killMongoInstance, launchReplicaSet, launchMongos, getRandomPort, generateMongoData} = require('src/mlaunch_utils');
 
 module.exports = {
   launchSingleInstance,
