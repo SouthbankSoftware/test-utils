ad41d8546fc2c339ef7909abd7bc26809834149a
Yi Zhao <zhaoyi0113@gmail.com>
2017-06-21 11:11:51 +1000

    use variable json template

--- src/mlaunch_utils.js
+++ src/mlaunch_utils.js
@@ -1,9 +1,9 @@
 const shelljs = require('shelljs');
 const os = require('os');
-const TMP_DIR = os.platform() !== 'win32' ? 'data': '/tmp/data';
+const TMP_DIR = os.platform() !== 'win32' ? 'data' : '/tmp/data';
 
-const MLAUNCH = os.platform() !== 'win32' ? 'mlaunch':'bash -c "mlaunch ';
-const MGENERATE = os.platform() !== 'win32'? 'mgenerate':'bash -c "mgenerate ';
+const MLAUNCH = os.platform() !== 'win32' ? 'mlaunch' : 'bash -c "mlaunch ';
+const MGENERATE = os.platform() !== 'win32' ? 'mgenerate' : 'bash -c "mgenerate ';
 
 /**
  * generate random port number between 6000 and 7000
@@ -30,7 +30,7 @@ const launchMongoInstance = (type, port, parameters) => {
     port +
     ' ' + '  --hostname localhost ' +
     parameters;
-  if(os.platform() === 'win32'){
+  if (os.platform() === 'win32') {
     command += '"';
   }
   console.log('launch command ', command);
@@ -64,6 +64,8 @@ const launchMongos = (port, nodenumber, parameters) => {
   launchMongoInstance('--mongos ' + nodenumber, port, parameters);
 };
 
+
+const templateJson = '{ "_id" : ObjectId("5949c7671b9ea95bdd6c46ef"), "user" : { "name" : { "last" : "King", "first" : "Ellie" }, "gender" : "female", "created_at" : ISODate("2010-03-04T04:50:20Z"), "is_active" : false, "address" : { "city" : "Brooklyn", "house_no" : 80, "street" : "oaVrcaeKVg", "zip_code" : 56602 }, "age" : 85 }, "tags" : [ { "id" : ObjectId("5949c7671b9ea95bdd6c46ea"), "subtags" : [ "iG5IKXQQfv", "LnR3xNKPaT", "eLHVdXOg2D", "PijR0Cdqwg", "kV8WNxQ8vX" ], "label" : "Joly4EuAlg" }, { "id" : ObjectId("5949c7671b9ea95bdd6c46eb"), "label" : "CG64zqpq8y" }, { "id" : ObjectId("5949c7671b9ea95bdd6c46ec"), "label" : "ucVTpyecuS" }, { "id" : ObjectId("5949c7671b9ea95bdd6c46ed"), "label" : "6ksf1rq5tm" }, { "id" : ObjectId("5949c7671b9ea95bdd6c46ee"), "label" : "ZR8pwsXQlS" } ] }';
 /**
  * generate mongo dump data on the collection
  *
@@ -72,7 +74,7 @@ const launchMongos = (port, nodenumber, parameters) => {
  * @param colName
  * @param parameters
  */
-const generateMongoData = (port, dbName = 'test', colName = 'test', templateFile, parameters = '') => {
+const generateMongoData = (port, dbName = 'test', colName = 'test', parameters = '') => {
   let command = MGENERATE + ' ' + templateFile + ' --num 1 --port ' +
     port +
     ' --database ' +
@@ -81,7 +83,7 @@ const generateMongoData = (port, dbName = 'test', colName = 'test', templateFile
     colName +
     ' ' +
     parameters;
-  if(os.platform() === 'win32'){
+  if (os.platform() === 'win32') {
     command += '"';
   }
   shelljs.exec(command);
@@ -96,7 +98,7 @@ const generateMongoData = (port, dbName = 'test', colName = 'test', templateFile
 const killMongoInstance = (port) => {
   const command = MLAUNCH + ' kill --dir ' + TMP_DIR + '/' + port;
   shelljs.exec(command);
-  os.platform() !== 'win32' ? shelljs.exec('rm -fr ' + TMP_DIR + '/' + port): shelljs.exec('bash -c "rm -fr ' + TMP_DIR + '/' + port + '"');
+  os.platform() !== 'win32' ? shelljs.exec('rm -fr ' + TMP_DIR + '/' + port) : shelljs.exec('bash -c "rm -fr ' + TMP_DIR + '/' + port + '"');
 };
 
 module.exports = {
